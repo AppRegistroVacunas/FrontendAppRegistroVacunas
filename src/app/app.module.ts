7c404c6d5aaffba40373f72b1440b89cca5433dc
Alexander <alshmex209@gmail.com>
2023-06-09 18:36:13 -0500

    feat: add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListWorkersComponent } from './list-workers/list-workers.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListWorkersComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,

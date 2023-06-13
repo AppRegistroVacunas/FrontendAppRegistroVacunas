343559b696fa2b52b9a7e9cbd9880f163b495ad7
Alexander <alshmex209@gmail.com>
2023-06-12 22:52:00 -0500

    feat: add components anda create methos crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterVaccinesComponent } from './register/components/add-vaccines/add-vaccines.component';
+import { VaccineTableComponent } from './tables/components/vaccine-table/vaccine-table.component';
+import { UpdateVaccinesComponent } from './update/components/update-vaccines/update-vaccines.component';
+import { RegisterComponent } from './register/register.component';
+import { PersonsTablesComponent } from './tables/components/persons-tables/persons-tables.component';
+import { AddPersonsComponent } from './register/components/add-persons/add-persons.component';
+import { UpdatePersonsComponent } from './update/components/update-persons/update-persons.component';
+import { VaccinesCenterComponent } from './tables/components/vaccines-center/vaccines-center.component';
+import { AddVaccinesCenterComponent } from './register/components/add-vaccines-center/add-vaccines-center.component';
+import { UpdateVaccinesCenterComponent } from './update/components/update-vaccines-center/update-vaccines-center.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'registrar-vacunas',
+  component:RegisterVaccinesComponent,
+  },
+  {
+    path:'registrar-personas',
+    component:AddPersonsComponent
+    },
+    {
+    path:'registrar-centros de vacunacion',
+    component:AddVaccinesCenterComponent
+    },
+  {path:'vacunas',
+   component:VaccineTableComponent},
+
+  {path:'personas',
+    component:PersonsTablesComponent},
+  {
+    path:'centros',
+    component:VaccinesCenterComponent
+  },
+  {path:'actualizar-vacuna/:id',
+   component:UpdateVaccinesComponent},
+   {
+    path:'actualizar-persona/:id',
+    component:UpdatePersonsComponent
+   },
+   {
+    path:'actualizar-centros/:id',
+    component:UpdateVaccinesCenterComponent
+   }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListWorkersComponent } from './list-workers/list-workers.component';
-import { MainPageComponent } from './main-page/main-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { RegisterVaccinesComponent } from './register/components/add-vaccines/add-vaccines.component';
+import { TablesComponent } from './tables/tables.component';
+import { VaccineTableComponent } from './tables/components/vaccine-table/vaccine-table.component';
+import { UpdateComponent } from './update/update.component';
+import { UpdateVaccinesComponent } from './update/components/update-vaccines/update-vaccines.component';
+import { RouterModule } from '@angular/router';
+import { AddPersonsComponent } from './register/components/add-persons/add-persons.component';
+import { PersonsTablesComponent } from './tables/components/persons-tables/persons-tables.component';
+import { UpdatePersonsComponent } from './update/components/update-persons/update-persons.component';
+import { VaccinesCenterComponent } from './tables/components/vaccines-center/vaccines-center.component';
+import { AddVaccinesCenterComponent } from './register/components/add-vaccines-center/add-vaccines-center.component';
+import { UpdateVaccinesCenterComponent } from './update/components/update-vaccines-center/update-vaccines-center.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListWorkersComponent,
-    MainPageComponent
+    RegisterComponent,
+    RegisterVaccinesComponent,
+    TablesComponent,
+    VaccineTableComponent,
+    UpdateComponent,
+    UpdateVaccinesComponent,
+    AddPersonsComponent,
+    PersonsTablesComponent,
+    UpdatePersonsComponent,
+    VaccinesCenterComponent,
+    AddVaccinesCenterComponent,
+    UpdateVaccinesCenterComponent,
   ],
   imports: [
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

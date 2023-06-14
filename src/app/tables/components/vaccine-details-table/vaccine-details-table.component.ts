f82eda74f3605e0b151cea7a779b0506ccd4b403
Alexander <alshmex209@gmail.com>
2023-06-14 14:33:10 -0500

    feat: add new components

---
+++ src/app/tables/components/vaccine-details-table/vaccine-details-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vaccine-details-table',
+  templateUrl: './vaccine-details-table.component.html',
+  styleUrls: ['./vaccine-details-table.component.scss']
+})
+export class VaccineDetailsTableComponent {
+
+}
